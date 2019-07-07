fb096524fe6315385425f354b25c8c6be17a0d36
Nick Bansal <extreme_wired@msn.com>
2019-07-07 16:33:44 +0100

    added in a keys array

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 class RomanNumeral {
   constructor() {
-    this.dictionary = {
+    this.romanNumeralValues = {
       1: 'I',
       2: 'II',
       3: 'III',
       4: 'IV',
+      5: 'V',
+      9: 'IX',
+      10: 'X',
     };
+    this.keys = Object.keys(this.romanNumeralValues).reverse();
   }
 
   convert(value) {
-    return this.dictionary[value];
+    let result = '';
+    let countValue = value;
+    this.keys.forEach((number) => {
+      if (number <= countValue) {
+        result += this.romanNumeralValues[number];
+        countValue -= number;
+      }
+    });
+    return result;
   }
 }
 
--- test.js
+++ test.js
@@ -15,4 +15,22 @@ describe('Roman numberal converter', () => {
   it('Should convert 4 into roman numeral', () => {
     expect(converter.convert(4)).toBe('IV');
   });
+  it('Should convert 5 into roman numeral', () => {
+    expect(converter.convert(5)).toBe('V');
+  });
+  it('Should convert 6 into roman numeral', () => {
+    expect(converter.convert(6)).toBe('VI');
+  });
+  it('Should convert 7 into roman numeral', () => {
+    expect(converter.convert(7)).toBe('VII');
+  });
+  it('Should convert 8 into roman numeral', () => {
+    expect(converter.convert(8)).toBe('VIII');
+  });
+  it('Should convert 9 into roman numeral', () => {
+    expect(converter.convert(9)).toBe('IX');
+  });
+  it('Should convert 10 into roman numeral', () => {
+    expect(converter.convert(10)).toBe('X');
+  });
 });
