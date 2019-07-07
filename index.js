7e138d0e81d98fb4470a58362108d5a47046209e
Nick Bansal <extreme_wired@msn.com>
2019-07-07 15:48:27 +0100

    added in a dictionary

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 class RomanNumeral {
-    convert(value) {
-        return "I"
-    }
+  constructor() {
+    this.dictionary = {
+      1: 'I',
+      2: 'II',
+      3: 'III',
+      4: 'IV',
+    };
+  }
+
+  convert(value) {
+    return this.dictionary[value];
+  }
 }
 
 module.exports = RomanNumeral;
--- test.js
+++ test.js
@@ -6,4 +6,13 @@ describe('Roman numberal converter', () => {
   it('Should convert 1 into a roman numeral', () => {
     expect(converter.convert(1)).toBe('I');
   });
+  it('Should convert 2 into roman numeral', () => {
+    expect(converter.convert(2)).toBe('II');
+  });
+  it('Should convert 3 into roman numeral', () => {
+    expect(converter.convert(3)).toBe('III');
+  });
+  it('Should convert 4 into roman numeral', () => {
+    expect(converter.convert(4)).toBe('IV');
+  });
 });
