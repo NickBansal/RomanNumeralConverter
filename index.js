68b25ee08c69ccf3662b276d80a860117a5d984b
Nick Bansal <extreme_wired@msn.com>
2019-07-07 15:34:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+class RomanNumeral {
+  constructor(options) {
+    this.options = options;
+  }
+
+  sayHi() {
+    return 'Hello';
+  }
+}
+
+module.exports = RomanNumeral;
---
+++ test.js
@@ -0,0 +1,7 @@
+const RomanNumeral = require('.');
+
+const converter = new RomanNumeral();
+
+describe('Roman numberal converter', () => {
+
+});
